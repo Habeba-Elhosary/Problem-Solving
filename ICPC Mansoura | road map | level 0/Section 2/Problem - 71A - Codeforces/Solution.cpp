143828c7858eff2cc14ac1aa9ca0f63d97c6865c
Habeba Elhosary <111078954+Habeba-Elhosary@users.noreply.github.com>
2024-02-15 02:17:30 +0200

    Uploaded "Problem - 71A - Codeforces" problem & solution

---
+++ ICPC Mansoura | road map | level 0/Section 2/Problem - 71A - Codeforces/Solution.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n ;
+    cin >> n ;
+    for(int i=0 ; i<n ; i++){
+        string x ;
+         cin >> x ;
+         if(x.length()<=10){
+             cout << x << endl ;
+         }else{
+             cout << x[0] << x.length()-2 << x[x.length()-1] << endl ;
+         }
+    }
+    return 0;
+}
