6dfbfe148276b764143c08d029d696157bb0254d
Habeba Elhosary <111078954+Habeba-Elhosary@users.noreply.github.com>
2024-02-18 19:16:29 +0200

    Uploaded "Problem - A3 - Codeforces" problem & solution

---
+++ ICPC Mansoura | road map | level 0/Section 3/Problem - A3 - Codeforces/Solution.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include<algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    string s , res = "" ;
+    cin >> s ;
+    for(int i=0 ; i<s.length() ; i++){
+        char x = tolower(s[i]) ;
+        if(x != 'a' && x != 'e' && x != 'i' && x != 'o' && x != 'u' && x != 'y') {
+            res += '.' ;
+            res += x ;
+        }
+    }
+    cout << res << endl;
+    return 0;
+ }
+ 
