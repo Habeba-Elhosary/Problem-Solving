3d7b84b97b4d4d44c0e2355b33ffbdb990a2d257
Habeba Elhosary <111078954+Habeba-Elhosary@users.noreply.github.com>
2024-02-20 02:17:23 +0200

    Uploaded "Problem - A4 - Codeforces" problem & solution

---
+++ ICPC Mansoura | road map | level 0/Section 3/Problem - A4 - Codeforces/Solution.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include<algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int w;
+    cin >> w;
+    if (w % 2 == 0 && w > 2) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
