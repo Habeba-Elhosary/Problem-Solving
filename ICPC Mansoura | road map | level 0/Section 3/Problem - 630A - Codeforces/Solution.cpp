a3e0c118fa2188145e79570bff3c56ef4f7a8aac
Habeba Elhosary <111078954+Habeba-Elhosary@users.noreply.github.com>
2024-02-20 19:48:43 +0200

    Uploaded "Problem - 630A - Codeforces" problem & solution

---
+++ ICPC Mansoura | road map | level 0/Section 3/Problem - 630A - Codeforces/Solution.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include<algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+    cout << 25 << endl;
+    return 0;
+}
