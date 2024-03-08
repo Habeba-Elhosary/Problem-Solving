cce777be2bda961df229d7bb648334de9744fe1e
Habeba Elhosary <111078954+Habeba-Elhosary@users.noreply.github.com>
2024-03-08 18:04:35 +0200

    Uploaded "Problem - B1 - Codeforces" problem & solution

---
+++ ICPC Mansoura | road map | level 0/Section 4/Problem - B1 - Codeforces/Solution.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    for (int i=s.size()-2 ; i>= 0; i=i-2) cout << s[i]; 
+    if (s.size() % 2 !=0 ) {
+        for (int i=0 ; i<s.size() ; i=i+2)
+            cout << s[i]; 
+    } else {
+        for (int i=1 ; i<s.size() ; i=i+2)
+            cout << s[i]; 
+    }
+    return 0;
+}
